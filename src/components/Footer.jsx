97b4916796cb98663bc03f6a1c426d461a875fda
ils-shubhamp <s.prakash@thesynapses.com>
2023-05-31 04:37:19 +0530

    add urls to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,16 +9,16 @@ export const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="/" target='_blank' className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+            <a href="https://www.instagram.com/shubh_am_prakash/" target='_blank' rel='noreferrer' className="mr-4 hover:underline md:mr-6 ">Instagram</a>
           </li>
           <li>
-            <a href="/" target='_blank' className="mr-4 hover:underline md:mr-6">Linked-In</a>
+            <a href="https://www.linkedin.com/in/shubham-prakash-0651581b3/" target='_blank' rel='noreferrer' className="mr-4 hover:underline md:mr-6">Linked-In</a>
           </li>
           <li>
-            <a href="/" target='_blank' className="mr-4 hover:underline md:mr-6">Youtube</a>
+            <a href="https://www.youtube.com/watch?v=szZrVN6UQM4&list=RDszZrVN6UQM4&start_radio=1" target='_blank' rel='noreferrer' className="mr-4 hover:underline md:mr-6">Youtube</a>
           </li>
           <li>
-            <a href="/" target='_blank' className="hover:underline">Github</a>
+            <a href="https://github.com/shubham075" target='_blank' rel='noreferrer' className="hover:underline">Github</a>
           </li>
         </ul>
       </div>
